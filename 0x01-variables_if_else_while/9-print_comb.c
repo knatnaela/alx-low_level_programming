9786507cdc4e3bec16f10e86a484b9026da1a1ff
Natnael Adane <natnaeladane21@gmail.com>
2021-06-18 08:29:42 -0700

    Add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+if (num == '9')
+{
+putchar(num);
+}
+else
+{
+putchar(num);
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
