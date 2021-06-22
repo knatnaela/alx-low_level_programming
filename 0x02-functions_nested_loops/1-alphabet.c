b6fafd9ed0da02ec99fbcb2dfdd53f74f4718221
Natnael Adane <knatnaelaadane@gmail.com>
2021-06-22 19:16:21 +0300

    Add 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /* more headers goes there */
 /**
-* main - Entry point
+* print_alphabet - Entry point
 *
 * Return: Always 0 (Success)
 */
 /* betty style doc for function main goes there */
-void print_alphabet()
+void print_alphabet(void)
 {
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
