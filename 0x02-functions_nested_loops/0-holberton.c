67b9d55020661aac13c201781db4a9a192314e11
Natnael Adane <knatnaelaadane@gmail.com>
2021-06-22 18:53:29 +0300

    Add 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,6 +1,14 @@
 #include "holberton.h"
-void main()
+int main()
 {
-_putchar("Holberton");
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 return(0);
 }
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,4 @@
 #ifndef _PUTCHAR
 #define _PUTCHAR
-
-int _putchar(char c); 
-
+int _putchar(char c);
 #endif
