68644e5e0aec3f6e792746b9d6ce63f252c830a6
Natnael Adane <knatnaelaadane@gmail.com>
2021-06-24 17:56:35 +0300

    Add 2-largest_number.c, holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,3 +6,9 @@
 void positive_or_negative(int i);
 
 #endif /* HOLBERTON_H */
+
+#ifndef LARGEST_NUMBER
+#define LARGEST_NUMBER
+void largest_number(int a, int b, int c);
+#endif /* LARGEST_NUMBER */
+
