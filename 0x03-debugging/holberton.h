e1b55976b2b440010723e4b52376efdd7af65606
Natnael Adane <knatnaelaadane@gmail.com>
2021-06-24 18:44:28 +0300

    Add 3-print_remaining_days.c, holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -12,3 +12,13 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 #endif /* LARGEST_NUMBER */
 
+#ifndef PRINT_REMAINING_DAYS
+#define PRINT_REMAINING_DAYS
+void print_remaining_days(int month, int day, int year);
+#endif /* PRINT_REMAINING_DAYS */
+
+#ifndef CONVERT_DAY
+#define CONVERT_DAY
+int convert_day(int month, int day);
+#endif /* CONVERT_DAY */
+
