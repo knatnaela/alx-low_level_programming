7110f8a7ea41a0f86b7b84e1a9278ebd5a57cbea
Natnael Adane <knatnaelaadane@gmail.com>
2021-06-24 20:38:19 +0300

    Add is digit

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,4 +1,25 @@
-#ifndef IS_UPPER
-#define IS_UPPER
+
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*
+ * File: holberton.h
+ * Auth: Natnael Adane
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
 int _isupper(int c);
-#endif /* IS_UPPER */
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
