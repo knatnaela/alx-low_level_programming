f5d9c6a6276da65255425e20bfbd701b54326dd5
Natnael Adane <knatnaelaadane@gmail.com>
2021-06-29 18:48:00 +0300

    Add 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,28 +1,47 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - check the code for Holberton School students.
- * 
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-char seed[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char password[22];
-int i, n, passw, complem;
-
+password[84];
+index = 0, sum = 0, diff_half1, diff_half2;
 srand(time(0));
-
-for (i = 0; passw < 2772; i++)
+while (sum < 2772)
 {
-n = rand() % 10;
-password[i] = seed[n];
-passw += password[i];
+password[index] = 33 + rand() % 94;
+sum += password[index++];
 }
-complem = 2772 - passw;
-password[i] = complem;
-printf("%s\n",  password);
+password[index] = '\0';
+if (sum != 2772)
+{
+diff_half1 = (sum - 2772) / 2;
+diff_half2 = (sum - 2772) / 2;
+if ((sum - 2772) % 2 != 0)
+diff_half1++;
+for (index = 0; password[index]; index++)
+{
+if (password[index] >= (33 + diff_half1))
+{
+password[index] -= diff_half1;
+break;
+}
+}
+for (index = 0; password[index]; index++)
+{
+if (password[index] >= (33 + diff_half2))
+{
+password[index] -= diff_half2;
+break;
+}
+}
+}
+printf("%s", password);
 return (0);
 }
