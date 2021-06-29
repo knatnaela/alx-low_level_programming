6f0be27119616b724449aadf0e7debecc37e33d0
Natnael Adane <knatnaelaadane@gmail.com>
2021-06-29 10:15:23 +0300

    Add 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+
+/*
+ * File: holberton.h
+ * Auth: Natnael Adane
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05. C - Pointers, arrays and strings directory.
+ */
+
+
+void reset_to_98(int *n);
+
+
+#endif
