4aa0d238265ad6bd67a39c952453d2e03f004a99
Natnael Adane <knatnaelaadane@gmail.com>
2021-07-01 21:57:09 +0300

    Add

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,12 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
