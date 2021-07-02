73bda3323befadc3188861c254630c47263085fa
Natnael Adane <knatnaelaadane@gmail.com>
2021-07-02 21:06:30 +0300

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,8 +1,3 @@
-/*
- * File: 102-infinite_add.c
- * Auth: Brennan D Baraban
- */
-
 #include "holberton.h"
 
 char *add_strings(char *n1, char *n2, char *r, int r_index);
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,8 +1,3 @@
-/*
- * File: 103-print_buffer.c
- * Auth: Brennan D Baraban
- */
-
 #include "holberton.h"
 #include <stdio.h>
 
