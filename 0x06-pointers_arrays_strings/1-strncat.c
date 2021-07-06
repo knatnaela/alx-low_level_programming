9124edd9d71dcaee693755542bafad832114aff0
Natnael Adane <knatnaelaadane@gmail.com>
2021-07-06 21:19:06 +0300

    Add

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,3 @@
-/*
- * File: 1-strncat.c
- * Auth: Brennan D Baraban
- */
-
 #include "holberton.h"
 
 /**
@@ -16,13 +11,10 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,3 @@
-/*
- * File: 2-strncpy.c
- * Auth: Brennan D Baraban
- */
-
 #include "holberton.h"
 
 /**
@@ -16,16 +11,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
-
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
-	return (dest);
+int index = 0, src_len = 0;
+while (src[index++])
+src_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[index] = src[index];
+for (index = src_len; index < n; index++)
+dest[index] = '\0';
+return (dest);
 }
